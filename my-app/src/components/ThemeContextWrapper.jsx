9980bd5fd88772810d095ade792dd02f24e32bc9
muhamadrizkibahtiar23 <muhamadrizkibahtiar230599@gmail.com>
2023-04-07 17:06:47 +0700

    tambah react

--- my-app/src/components/ThemeContextWrapper.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ThemeContextWrapper() {
-  return (
-    <div>
-      <button className="toggle">light</button>
-    </div>
-  )
-}
--- my-app/src/pages/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer () {
-  return (
-    <footer className="py-4 d-flex justify-content-center align-items-center bg-secondary">
-      &copy; Muhamad Rizki Bahtiar
-    </footer>
-  );
-}
